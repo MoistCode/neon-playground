565f9111d7218553864374dfcdfadd5341c478d7
Tommy Pham <phamtommylong1991@gmail.com>
2019-08-26 15:15:26 -0700

    test

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,11 +1,20 @@
 #[macro_use]
 extern crate neon;
+extern crate regex;
 
 use neon::prelude::*;
+use regex::Regex;
+use std::fmt::Debug::fmt;
 
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
+    println!("{:?}", cx);
     Ok(cx.string("hello node"))
 }
+
+// fn fetch_emails(mut cx: FunctionContext) -> JsResult<JsArray> {
+//     let re = Regex::new(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)").unwrap();
+//     assert!(re.is_match(""))
+// }
 
 register_module!(mut cx, {
     cx.export_function("hello", hello)
